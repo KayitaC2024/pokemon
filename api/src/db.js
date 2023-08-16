69a40f6bbc3ee18a9b3110abe74b5da705db991b
kayitaC2024 <kayita_y@hotmail.com>
2023-08-16 10:20:44 -0500

    deployyy

--- api/src/db.js
+++ api/src/db.js
@@ -1,8 +1,8 @@
 require('dotenv').config();
-const { Sequelize } = require('sequelize');
+const { Sequelize, op } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
-const { DB_USER, DB_PASSWORD, DB_HOST, PGPASSWORD_DEPLOY } = process.env;
+const { DB_USER, DB_PASSWORD, DB_HOST,DB_DEPLOY } = process.env;
 
 // const sequelize = new Sequelize(
 //    `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/pokemon`,
@@ -12,7 +12,7 @@ const { DB_USER, DB_PASSWORD, DB_HOST, PGPASSWORD_DEPLOY } = process.env;
 //    }
 // );
 
-var sequelize = new Sequelize(PGPASSWORD_DEPLOY, {
+const sequelize = new Sequelize(DB_DEPLOY, {
       logging: false, // set to console.log to see the raw SQL queries
       native: false, // lets Sequelize know we can use pg-native for ~30% more speed
    });
--- client/src/index.js
+++ client/src/index.js
@@ -35,8 +35,8 @@ import axios from "axios";
 dotenv.config();
 
 //axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001"; ya estaba/quimbay
-//axios.defaults.baseURL ="http://localhost:3001";// es avisarle a axios que todas las peticiones empiezen o tengan una misma base url(front)para trabajar con el proyecto de forma local utilizamos este para las peticiones
-axios.defaults.baseURL = 'https://pokemonback-production.up.railway.app/';// conectar front con el back que ya deployamos// cuando queramos puchear o actualizar nuestro deploy del front  lo hacemos aca 
+axios.defaults.baseURL ="http://localhost:3001";// es avisarle a axios que todas las peticiones empiezen o tengan una misma base url(front)para trabajar con el proyecto de forma local utilizamos este para las peticiones
+//axios.defaults.baseURL = 'https://pokemonback-production.up.railway.app/';// conectar front con el back que ya deployamos// cuando queramos puchear o actualizar nuestro deploy del front  lo hacemos aca 
 
 
 ReactDOM.render(
